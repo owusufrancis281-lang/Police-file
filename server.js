72fe7b4b7d501dbf759adedc8af54551e62df89b
owusufrancis281-lang <owusufrancis281@gmail.com>
2026-07-16 18:00:08 +0000

    Remove welcome route and serve static files
    Removed welcome route and added static file serving.

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Welcome to Police-file!");
-});
+// Tell Express to show files inside the "public" folder
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
